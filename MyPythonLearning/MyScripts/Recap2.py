f068f40bc222e6e32bbc94bb2afdf26a1fc710f3
DayanaCil <Dayanacil26@gmail.com>
2020-03-16 22:34:35 +0530

    Recap2 Exercise

---
+++ MyPythonLearning/MyScripts/Recap2.py
@@ -0,0 +1,8 @@
+get the below as output
+
+#     "Hi there 'banofee'
+# I want you so much  now \:-\
+# "
+#
+# Take an input of name from the user and print the name as below
+# "Hi name,your reverse is eman,1st letter is n and last letter is e"
