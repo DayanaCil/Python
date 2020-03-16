0c76d6a639e31d97ac90f1cfbf824e119cb60663
KiranChilledOut <36853757+KiranChilledOut@users.noreply.github.com>
2020-03-16 22:36:55 +0530

    Update Recap.py

---
+++ MyPythonLearning/MyScripts/Recap.py
@@ -0,0 +1,6 @@
+# convert 39 to binary and back
+# remainder
+# exponent
+#
+# input string to integer and multiply the integer resulwith 11 and output .Convert the output to string and print it.
+#
